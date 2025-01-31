de088d3c369eb6b08ca92d155d0d574e37f87b23
AKaash <akashthampuran1217@gmail.com>
2025-01-31 13:14:00 +0530

    change to nest js

--- models/banner.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const bannerSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  image: { type: String, required: true },
-  link: { type: String },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Banner', bannerSchema);
