fe0262ccf42d83472755bffef3fc0aff9ea195df
Rahul <torahulsomaraj@gmail.com>
2025-02-01 13:58:34 +0530

    chnage in refreshgh service token

--- src/auth/auth.refresh.service.ts
+++ src/auth/auth.refresh.service.ts
@@ -1,28 +1,20 @@
-// refresh-token.service.ts
-
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-
-import * as bcrypt from 'bcrypt';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { AuthTokens } from './schemas/schema.refresh-token';
 
 @Injectable()
 export class RefreshTokenService {
   constructor(
-    @InjectRepository(AuthTokens)
-    private refreshTokenRepository: Repository<AuthTokens>,
+    @InjectModel(AuthTokens.name) private refreshTokenModel: Model<AuthTokens>,
   ) {}
 
-  async createRefreshToken(userId: number, token: string): Promise<AuthTokens> {
-    const refreshToken = this.refreshTokenRepository.create({
-      userId,
-      token,
-    });
-    return this.refreshTokenRepository.save(refreshToken);
+  async createRefreshToken(userId: string, token: string): Promise<AuthTokens> {
+    const refreshToken = new this.refreshTokenModel({ userId, token });
+    return refreshToken.save();
   }
 
   async removeRefreshToken(token: string): Promise<void> {
-    await this.refreshTokenRepository.delete({ token });
+    await this.refreshTokenModel.deleteOne({ token }).exec();
   }
 }
